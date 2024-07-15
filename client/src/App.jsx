ea668cb295b1dfaa5bebfaa66360b6243fb18863
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-07-15 10:54:01 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+ 
+      <>
+      <h1 className='text-3xl text-blue-500'>Aplicación</h1>
+      <p>Esto es una prueba</p>
+      </>
+ 
+  )
+}
