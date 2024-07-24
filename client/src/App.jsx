bc44f6b0f02ab1be25cf2c61caa8369f126b2f38
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-07-24 10:37:57 +0200

    frontend_pages001_creating sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 export default function App() {
   return (
  
-      <>
-      <h1 className='text-3xl text-blue-500'>Aplicación</h1>
-      <p>Esto es una prueba</p>
-      </>
+    <BrowserRouter>
+    <ScrollToTop />
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      {/* <Route path='/search' element={<Search />} /> */}
+      <Route path='/dashboard' element={<Dashboard />} />
+      {/* <Route element={<PrivateRoute />}>
+      </Route>
+      <Route element={<OnlyAdminPrivateRoute />}>
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/update-post/:postId' element={<UpdatePost />} />
+      </Route>
+
+      <Route path='/projects' element={<Projects />} />
+      <Route path='/post/:postSlug' element={<PostPage />} /> */}
+    </Routes>
+    <Footer /> 
+  </BrowserRouter>
  
   )
 }
