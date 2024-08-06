2d0339a8772fc60f716a5fb0f4f8910e881a4b7d
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-08-06 11:32:12 +0200

    frontend_user011_User management complete

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {
   deleteUser,
-  // getUser,
-  // getUsers,
+  getUser,
+  getUsers,
   signout,
   test,
   updateUser,
@@ -16,6 +16,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
-// router.get('/:userId', getUser);
+router.get('/:userId', getUser);
 
 export default router;
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -69,12 +69,12 @@ export default function DashUsers() {
         <>
           <Table hoverable className='shadow-md'>
             <Table.Head>
-              <Table.HeadCell>Date created</Table.HeadCell>
-              <Table.HeadCell>User image</Table.HeadCell>
-              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Fecha de creación</Table.HeadCell>
+              <Table.HeadCell>Imagen de usuario</Table.HeadCell>
+              <Table.HeadCell>Nombre</Table.HeadCell>
               <Table.HeadCell>Email</Table.HeadCell>
               <Table.HeadCell>Admin</Table.HeadCell>
-              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>Borrar</Table.HeadCell>
             </Table.Head>
             {users.map((user) => (
               <Table.Body className='divide-y' key={user._id}>
@@ -106,7 +106,7 @@ export default function DashUsers() {
                       }}
                       className='font-medium text-red-500 hover:underline cursor-pointer'
                     >
-                      Delete
+                      Borrar
                     </span>
                   </Table.Cell>
                 </Table.Row>
@@ -118,12 +118,12 @@ export default function DashUsers() {
               onClick={handleShowMore}
               className='w-full text-teal-500 self-center text-sm py-7'
             >
-              Show more
+              Mostrar más
             </button>
           )}
         </>
       ) : (
-        <p>You have no users yet!</p>
+        <p>No tienes usuarios todavia</p>
       )}
       <Modal
         show={showModal}
@@ -136,14 +136,14 @@ export default function DashUsers() {
           <div className='text-center'>
             <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
             <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
-              Are you sure you want to delete this user?
+              ¿Seguro que quieres borrar este usuario?
             </h3>
             <div className='flex justify-center gap-4'>
               <Button color='failure' onClick={handleDeleteUser}>
-                Yes, I'm sure
+                Si
               </Button>
               <Button color='gray' onClick={() => setShowModal(false)}>
-                No, cancel
+                No
               </Button>
             </div>
           </div>
